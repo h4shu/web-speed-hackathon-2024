d16085f4d00f5af591074c8591269c26f4f8523b
h4shu <12460080+h4shu@users.noreply.github.com>
2024-03-24 16:21:04 +0900

    try to revart for service worker issue

--- workspaces/server/tsup.config.ts
+++ workspaces/server/tsup.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(async (): Promise<Options[]> => {
       },
       env: {
         API_URL: process.env['RENDER_EXTERNAL_URL'] ?? 'http://localhost:10000',
-        NODE_ENV: 'production',
+        NODE_ENV: process.env['NODE_ENV'] || 'development',
       },
       format: 'cjs',
       metafile: true,
